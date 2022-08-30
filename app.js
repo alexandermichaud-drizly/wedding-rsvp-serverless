270d667a513e75ecc734cec443a56615a1b2ff38
ARM <alexander.michaud@aya.yale.edu>
2022-08-30 06:38:19 -0400

    Post works!

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const { Rsvp, sequelize }= require("./rsvp_model");
 const { Op } = require("sequelize");
 const express = require("express");
+const boolParser = require('express-query-boolean');
 const app = express();
 const { isNil } = require("lodash");  
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(boolParser());
 
 app.get("/test-api", (req, res, next) => res.status(200).json({ message: "great success"}));
 
@@ -24,26 +29,34 @@ app.get("/guest", async (req, res, next) => {
   const { first_name, last_name } = query;
 
   if (first_name && last_name) {
-    const matches = await Rsvp.findAll({ where: { first_name: {
-      [Op.like]: `${first_name}%`
-    }, last_name: {
-      [Op.like]: `${last_name}%`
-    } }});
-    return res.status(200).json({
-      matches,
-    });
+    try {
+      const matches = await Rsvp.findAll({ where: { first_name: {
+        [Op.like]: `${first_name}%`
+      }, last_name: {
+        [Op.like]: `${last_name}%`
+      } }});
+      return res.status(200).json({
+        matches,
+      });
+    } catch (error) {
+      next(error)  
+    }
   }
   return res.status(400).json({ message: "Full name not provided"});
 });
 
 app.post("/reply", async (req, res, next) => {
-  const { query } = req;
-  const { guest_id, attending } = query;
-  if (guest_id && !isNil(attending)) {
-    const result = await Rsvp.update({ attending: !!attending }, { where: { guest_id }});
-    return res.status(200).json({
-      message: result,
-    });
+  const { body } = req;
+  const { guest_id, attending } = body;
+  if (guest_id && !isNil(attending)) { 
+    try { 
+      const result = await Rsvp.update({ attending }, { where: { guest_id }});
+      return res.status(200).json({
+        message: result,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
   return res.status(400).json({ query }); 
 });
@@ -56,8 +69,8 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   return res.status(500).json({
-    message: error,
+    message: err,
   });
-})
+});
 
 module.exports.app = app;
